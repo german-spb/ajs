6fdd0930ed997dd665ec59279956f4bae2db2cad
German <german@pushkin-spb.com>
2023-10-22 17:05:29 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-function info() {
-    const m = new Map();
-    m.set('name', 'ajs');
-    m.set('version', '1.0.0');
-    return m;
-  }
-  
-  export default {
-    info
-  };
+const ajs = require('@german-spb/ajs');
+
+console.log(ajs.info());
